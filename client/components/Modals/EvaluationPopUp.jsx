41fb010c46a0d6550e0667a566204ae9b1391bd8
krishchopra02 <krish.ug20@nsut.ac.in>
2023-10-09 03:58:02 +0530

    Added embeddings

--- client/components/Modals/EvaluationPopUp.jsx
+++ client/components/Modals/EvaluationPopUp.jsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { ImCross } from "react-icons/im";
+import React from 'react'
+import { ImCross } from 'react-icons/im'
 
 export default function EvaluationPopUp({
   question,
   correctAnswer,
   score,
+  userAnswer,
   setopenPopUp,
 }) {
   return (
-    <div className="z-50 h-screen w-screen fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center overflow-y-hidden">
-      <div className="h-5/6 w-1/2 p-6 bg-gray-50 flex items-center justify-center rounded-lg">
-        <div className="h-full container max-w-screen-lg mx-auto">
+    <div className='z-50 h-screen w-screen fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center overflow-hidden'>
+      <div className='h-5/6 w-1/2 p-6 bg-gray-50 flex items-center justify-center rounded-lg'>
+        <div className='h-full container max-w-screen-lg'>
           <div>
-            <h2 className="font-semibold text-xl flex justify-between items-center text-gray-600 mb-5">
+            <h2 className='font-semibold text-xl flex justify-between items-center text-gray-600 mb-5'>
               <div></div>
               <div>
                 <button
-                  className="hover:text-slate-500"
+                  className='hover:text-slate-500'
                   onClick={() => setopenPopUp(false)}
                 >
                   <ImCross />
@@ -24,20 +25,24 @@ export default function EvaluationPopUp({
               </div>
             </h2>
             <div>
-              <span className="font-bold pr-2">Question :</span>
+              <span className='font-bold pr-2'>Question :</span>
               {question}
             </div>
-            <div className="pt-3">
-              <div className="font-bold">Expected Answer : </div>
-              <div>{correctAnswer}s</div>
+            <div className='pt-3 h-52 overflow-y-scroll w-full'>
+              <div className='font-bold'>Expected Answer : </div>
+              <div>{correctAnswer}</div>
             </div>
-            <div className="mt-6">
-              <div className="mt-2 bg-gray-600 rounded-full">
+            <div className='pt-3 overflow-y-scroll h-52 w-full'>
+              <div className='font-bold'>Your Answer : </div>
+              <div>{userAnswer}</div>
+            </div>
+            <div className='mt-6'>
+              <div className='mt-2 bg-gray-600 rounded-full'>
                 <div
-                  className="mt-2 bg-blue-500 text-center rounded-full"
+                  className='mt-2 bg-blue-500 text-center rounded-full'
                   style={{ width: `${score}%` }}
                 >
-                  <div className="text-white text-sm inline-block bg-blue-500 px-2 rounded-full">
+                  <div className='text-white text-sm inline-block bg-blue-500 px-2 rounded-full'>
                     {score}% correct answer
                   </div>
                 </div>
@@ -47,5 +52,5 @@ export default function EvaluationPopUp({
         </div>
       </div>
     </div>
-  );
+  )
 }
